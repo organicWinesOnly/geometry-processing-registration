78257d31934299cd029d4e17d8fbc569f65e06e8
rhamel.mw <rhamel.rdelpeache@gmail.com>
2020-10-13 01:46:16 -0400

    Add Rotation Approximation function

--- src/closest_rotation.cpp
+++ src/closest_rotation.cpp
@@ -1,9 +1,21 @@
 #include "closest_rotation.h"
+#include <Eigen/LU>
+#include <Eigen/SVD>
 
 void closest_rotation(
   const Eigen::Matrix3d & M,
   Eigen::Matrix3d & R)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
+  // Find the SVD of M
+  Eigen::JacobiSVD<Eigen::MatrixXd> svd(M, Eigen::ComputeFullU | Eigen::ComputeFullV);
+  Eigen::MatrixXd UV = svd.matrixU() * svd.matrixV().transpose();
+
+  // compute the special matrix omega
+  double omega_0;
+  omega_0 = UV.determinant();
+  Eigen::Matrix3d omega = Eigen::Matrix3d::Identity();
+  omega(2, 2) = omega_0;
+
+  // Solve for R
+  R = svd.matrixU() * omega * svd.matrixV();
 }
