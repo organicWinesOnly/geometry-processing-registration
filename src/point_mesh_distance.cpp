cc537e7405e6b9a7ed73fb7cab3c51af64e8ca46
rhamel.mw <rhamel.rdelpeache@gmail.com>
2020-10-13 00:12:15 -0400

    Add mesh_distance function

--- src/point_mesh_distance.cpp
+++ src/point_mesh_distance.cpp
@@ -1,4 +1,7 @@
 #include "point_mesh_distance.h"
+#include "igl/per_face_normals.h"
+#include <cmath>
+#include <iostream>
 
 void point_mesh_distance(
   const Eigen::MatrixXd & X,
@@ -8,9 +11,33 @@ void point_mesh_distance(
   Eigen::MatrixXd & P,
   Eigen::MatrixXd & N)
 {
-  // Replace with your code
+  N.resizeLike(X);
   P.resizeLike(X);
-  N = Eigen::MatrixXd::Zero(X.rows(),X.cols());
-  for(int i = 0;i<X.rows();i++) P.row(i) = VY.row(i%VY.rows());
-  D = (X-P).rowwise().norm();
+  D.resize(X.rows());
+  Eigen::RowVector3d z;
+  z.fill(1 / std::sqrt(3));
+  Eigen::MatrixXd normals(FY.rows(), 3);
+
+  // Find the normals of the every face in the mesh
+  igl::per_face_normals(VY, FY, z, normals);
+
+  // find the projected points
+  Eigen::MatrixXd x_projections(normals.rows(), X.rows());
+  x_projections = normals * X.transpose();
+
+  // Find the closest face to each point in X
+  for (int i = 0; i < X.rows(); i++)
+  {
+    int *idx;
+    int index;
+
+    idx = &index;
+    double max_proj;
+    max_proj = x_projections.col(i).maxCoeff(idx);
+    N.row(i) = normals.row(*idx);
+    P.row(i) =  max_proj * normals.row(*idx);
+  }
+
+  // distance calculation
+  D = (X - P).rowwise().norm();
 }
